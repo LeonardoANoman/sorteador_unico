f8170a2e2a74e105263ae497e94b27fcbb4dc71f
Leonardo Antunes <leonoman07@gmail.com>
2022-10-27 17:34:53 -0300

    sorteador brabo

---
+++ sorteador.py
@@ -0,0 +1,10 @@
+from random import randint
+num_max = 28
+num_sorteados = []
+
+while len(num_sorteados) < 28:
+    num_sorteado = randint(1, num_max)
+    if num_sorteado not in num_sorteados:
+        num_sorteados.append(num_sorteado)
+
+print(num_sorteados)
