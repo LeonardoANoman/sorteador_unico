b47821a3b204f339e96ea82e7b7843767bf2d4df
Leonardo <leonoman07@gmail.com>
2022-12-27 15:55:40 -0300

    oopsie

--- sorteador.py
+++ sorteador.py
@@ -2,7 +2,7 @@ from random import randint
 num_max = 28
 num_sorteados = []
 
-while len(num_sorteados) < 28:
+while len(num_sorteados) < num_max:
     num_sorteado = randint(1, num_max)
     if num_sorteado not in num_sorteados:
         num_sorteados.append(num_sorteado)
